697bf0a6b61d99ed791b382f343c9404bfc141d3
Dmitry Dumanov <dumanov1996@mail.ru>
2020-08-17 22:35:55 +0300

    1

--- ..scripts/script.js
+++ ..scripts/script.js
@@ -5,6 +5,7 @@ window.onload = function(){
    button.addEventListener( 'click', SayHello)
    function SayHello(){
       var h1 = document.querySelector(".h1");
-      h1.innerHTML = "Привет Бабей!!!"
+      h1.innerHTML += "Привет Бабей!!!"
+      console.log(h1.value)
    }
 }
